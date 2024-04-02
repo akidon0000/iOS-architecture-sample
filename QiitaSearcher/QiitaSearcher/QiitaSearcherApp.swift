50540f361de2050c6728ca894f3895fc3f832380
akidon0000 <akidon0000@gmail.com>
2024-04-02 23:54:27 +0900

    init settings

---
+++ QiitaSearcher/QiitaSearcher/QiitaSearcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  QiitaSearcherApp.swift
+//  QiitaSearcher
+//
+//  Created by Akihiro Matsuyama on 2024/04/02.
+//
+
+import SwiftUI
+
+@main
+struct QiitaSearcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
